c10c6f51ff13c630c21c6a24fe49fa45e482c149
Jung-Haeng Lee <jhlee@vcnc.co.kr>
2012-05-07 12:42:03 +0900

    server address

--- athens-example-client/src/net/rothlee/athens/android/OlympusConst.java
+++ athens-example-client/src/net/rothlee/athens/android/OlympusConst.java
@@ -21,7 +21,7 @@ package net.rothlee.athens.android;
 public interface OlympusConst {
 
 //	public static final String SERVER_HOST = "http://rothlee.net:8080";
-	public static final String SERVER_HOST = "http://172.30.1.1:8080";
+	public static final String SERVER_HOST = "https://rothlee.net/athens";
 	
 	public static final String PATH_GET_ACCEESS_TOKEN = "/getAccessToken";
 	public static final String PATH_WRITE = "/write";
